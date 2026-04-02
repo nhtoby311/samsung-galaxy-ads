1afc0265ea5138f39720d1943681650f8cf8d7e3
nvminhhieu <nvminhhieu1999@gmail.com>
2026-04-02 02:17:52 +0200

    lint

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
 
-console.clear()
+console.clear();
 console.log(
-  '%c✨ Made with ❤️ by nhtoby.com ✨',
-  'font-size:16px;font-weight:bold;color:#b9beff;background:#1a1a2e;padding:8px 16px;border-radius:8px;',
-  '\nhttps://nhtoby.com',
-)
+	'%c✨ Made with ❤️ by nhtoby.com ✨',
+	'font-size:16px;font-weight:bold;color:#b9beff;background:#1a1a2e;padding:8px 16px;border-radius:8px;',
+	'\nhttps://nhtoby.com',
+);
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+	<StrictMode>
+		<App />
+	</StrictMode>,
+);
