b67cee8773c6d7455e3eb29890dfc16ba42c6a3b
nvminhhieu <nvminhhieu1999@gmail.com>
2026-04-14 13:17:03 +0200

    format

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,9 @@ import TopOverlay from './components/OverlayLayout/TopOverlay';
 function App() {
 	const controlsRef = useRef<CameraControls>(null);
 	const sceneVisible = useAppStore((s) => s.sceneVisible);
-	const levaHidden = !new URLSearchParams(window.location.search).has('debug');
+	const levaHidden = !new URLSearchParams(window.location.search).has(
+		'debug',
+	);
 
 	// HACK: Bc after .glb animated, object shift away from 0,0,0. So shift target to orbit obj
 	useEffect(() => {
